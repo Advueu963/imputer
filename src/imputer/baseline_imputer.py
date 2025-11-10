cd481c83bbc34e9f16dbfe90d625ddce489682cb
MrSkooma <maximilianneumann1999@outlook.de>
2025-11-10 17:27:32 +0100

    created first base impute for Satic of the np.ndarray

---
+++ src/imputer/baseline_imputer.py
@@ -0,0 +1,36 @@
+import lazy_dispatch as ld
+import numpy as np
+from enum import Enum
+
+class ImputeMode(Enum):
+    STATIC = "static"
+    MEAN = "mean"
+    MEDIAN = "median"
+
+def impute(point : object, reference : object, colations : object, mode=ImputeMode.STATIC) -> object:
+    if mode == ImputeMode.STATIC:
+        return impute_static(point, reference, colations)
+    else:
+        raise NotImplementedError(f"Imputation mode {mode} not implemented.")
+
+@ld.lazydispatch
+def impute_static(point : object, reference : object, colations : object) -> object:
+    return None
+
+@impute_static.register(np.ndarray)
+def impute_static_np(point: np.ndarray, reference: np.ndarray, colations: np.ndarray) -> np.ndarray:
+    imputed = point.copy()
+    for i in range(point.shape[0]):
+        if colations[i]:
+            imputed[i] = reference[i]
+    return imputed
+if __name__ == "__main__":
+
+    point = np.ndarray(shape=(4,), dtype=int)
+    point[:] = [4, 5, 6, 7]
+    reference = np.ndarray(shape=(4,), dtype=int)
+    reference[:] = [9, 8, 7, 6]
+    colations = np.ndarray(shape=(4,), dtype=int)
+    colations[:] = [0, 1, 0, 1]
+    print(colations.size)
+    print(impute(point, reference, colations, mode=ImputeMode.STATIC))
