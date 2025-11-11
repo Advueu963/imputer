1ebc0b4af19b7fd912e17db09b984040e02da92e
Bastian Hunecke <bastianhunecke@gmail.com>
2025-11-11 11:15:43 +0100

    add mean and median for jax arrays

--- src/imputer/baseline_imputer.py
+++ src/imputer/baseline_imputer.py
@@ -21,6 +21,15 @@ def impute(point : object, reference : object, colations : object, mode=ImputeMo
 def impute_static(point : object, reference : object, colations : object) -> object:
     return None
 
+@ld.lazydispatch
+def impute_mean(point : object, reference : object, colations : object) -> object:
+    return None
+
+@ld.lazydispatch
+def impute_median(point : object, reference : object, colations : object) -> object:
+    return None
+
+# Implementations for numpy arrays
 @impute_static.register(np.ndarray)
 def impute_static_np(point: np.ndarray, reference: np.ndarray, colations: np.ndarray) -> np.ndarray:
     imputed = point.copy()
@@ -28,10 +37,6 @@ def impute_static_np(point: np.ndarray, reference: np.ndarray, colations: np.nda
         if colations[i]:
             imputed[i] = reference[i]
     return imputed
-
-@ld.lazydispatch
-def impute_mean(point : object, reference : object, colations : object) -> object:
-    return None
 
 @impute_mean.register(np.ndarray)
 def impute_mean_np(point: np.ndarray, reference: np.ndarray, colations: np.ndarray) -> np.ndarray:
@@ -42,10 +47,6 @@ def impute_mean_np(point: np.ndarray, reference: np.ndarray, colations: np.ndarr
             imputed[i] = mean_value[i]
     return imputed
 
-@ld.lazydispatch
-def impute_median(point : object, reference : object, colations : object) -> object:
-    return None
-
 @impute_median.register(np.ndarray)
 def impute_median_np(point: np.ndarray, reference: np.ndarray, colations: np.ndarray) -> np.ndarray:
     imputed = point.copy()
@@ -55,8 +56,7 @@ def impute_median_np(point: np.ndarray, reference: np.ndarray, colations: np.nda
             imputed[i] = median_value[i]
     return imputed
 
-
-
+# Implementations for jax arrays
 @impute_static.register("jax.Array")
 def impute_static_jax(point: "jax.Array", reference: "jax.Array", colations: "jax.Array") -> "jax.Array":
     imputed = point.copy()
@@ -65,8 +65,29 @@ def impute_static_jax(point: "jax.Array", reference: "jax.Array", colations: "ja
             imputed = imputed.at[i].set(reference[i])
     return imputed
 
+@impute_mean.register("jax.Array")
+def impute_mean_jax(point: "jax.Array", reference: "jax.Array", colations: "jax.Array") -> "jax.Array":
+    from jax import numpy as jnp
+    imputed = point.copy()
+    mean_value = jnp.mean(reference, axis=1).astype(point.dtype)
+    for i in range(point.shape[0]):
+        if colations[i]:
+            imputed = imputed.at[i].set(mean_value[i])
+    return imputed
+
+@impute_median.register("jax.Array")
+def impute_median_jax(point: "jax.Array", reference: "jax.Array", colations: "jax.Array") -> "jax.Array":
+    from jax import numpy as jnp
+    imputed = point.copy()
+    median_value = jnp.median(reference, axis=1).astype(point.dtype)
+    for i in range(point.shape[0]):
+        if colations[i]:
+            imputed = imputed.at[i].set(median_value[i])
+    return imputed
+
 if __name__ == "__main__":
-    # Test data    point = np.array([4.0, 5.0, 6.0, 7.0])
+    # Test data
+    point = np.array([4.0, 5.0, 6.0, 7.0])
     reference = np.array([9.0, 8.0, 7.0, 6.0])
     reference_matrix = np.array([
         [10.0, 1.0, 5.0, 3.0],
@@ -99,8 +120,26 @@ if __name__ == "__main__":
     print(f"Result: {result_median}")
 
     import jax.numpy as jnp
-    point_jax = jnp.array([4, 5, 6, 7])
-    reference_jax = jnp.array([9, 8, 7, 6])
+    point_jax = jnp.array([4.0, 5.0, 6.0, 7.0])
+    reference_jax = jnp.array([9.0, 8.0, 7.0, 6.0])
+    reference_matrix_jax = jnp.array([
+        [10.0, 1.0, 5.0, 3.0],
+        [20.0, 2.0, 5.0, 6.0],
+        [30.0, 9.0, 5.0, 9.0],
+        [40.0, 6.0, 5.0, 12.0]
+    ])
     colations_jax = jnp.array([0, 1, 0, 1])
     print(colations_jax.size)
     print(impute(point_jax, reference_jax, colations_jax, mode=ImputeMode.STATIC))
+
+    # Test Mean
+    reference_matrix_jax = jnp.array([
+        [10, 1, 5, 3],
+        [20, 2, 5, 6],
+        [30, 9, 5, 9],
+        [40, 6, 5, 12]
+    ])
+    print(impute(point_jax, reference_matrix_jax, colations_jax, mode=ImputeMode.MEAN))
+
+    # Test Median
+    print(impute(point_jax, reference_matrix_jax, colations_jax, mode=ImputeMode.MEDIAN))
