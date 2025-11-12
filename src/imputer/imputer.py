f90fbbb56d80843340455cf31cd6a93e6841ae3f
Bastian Hunecke <bastianhunecke@gmail.com>
2025-11-12 10:32:12 +0100

    update Imputer class

--- src/imputer/imputer.py
+++ src/imputer/imputer.py
@@ -1,10 +1,12 @@
+from abc import abstractmethod
+
 class Imputer():
-    def __init__(self):
+    def __init__(self) -> None:
         pass
 
-    def __call__(self, coalitions):
+    def __call__(self, coalitions, model=None):
         imputed_data = self.impute(coalitions)
-        predictions = self.predict(imputed_data)
+        predictions = self.predict(imputed_data, model=model)
         outputs = self.postprocess(predictions)
         return outputs
 
@@ -12,10 +14,12 @@ class Imputer():
     def impute(self, coalitions):
         raise NotImplementedError("Subclasses must implement this method")
 
-    def predict(self, imputed_data):
-        outputs_pred = [] # placeholder for model predictions
-        return outputs_pred
+    def predict(self, imputed_data, model):
+        if model:
+            outputs_pred = model.predict(imputed_data) # TODO: adapt to model interface
+            return outputs_pred
+        else:
+            raise ValueError("Model must be provided for prediction")
 
     def postprocess(self, predictions):
-        outputs_post = predictions # placeholder for postprocessing
-        return outputs_post
+        return predictions #TODO: placeholder for postprocessing
